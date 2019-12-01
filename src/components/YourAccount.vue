c7e5d6c5964070110313d31220997f2b09fbe9a8
dung-phan <madewithcrayons@gmail.com>
2019-12-01 15:17:18 +0100

    render layout in booklist component with bootstrap

--- src/components/YourAccount.vue
+++ src/components/YourAccount.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Hello from my account</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          NAME: PRICE
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input type="number" class="form-control" placeholder="Quantity" />
+        </div>
+        <div class="pull-right">
+          <button class="btn btn-success">
+            Select
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
