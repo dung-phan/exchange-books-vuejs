7757e46b389b23bae476736050809e975ea861e5
dung-phan <madewithcrayons@gmail.com>
2019-12-02 19:22:05 +0100

    update user cart

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,51 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link to="/" class="navbar-brand">HOME</router-link>
-
-    <div class="collapse navbar-collapse">
-      <ul class="nav navbar-nav">
-        <router-link to="/list" activeClass="active" tag="li">
-          <a>BOOK LIST</a></router-link
-        >
-        <router-link to="/account" activeClass="active" tag="li">
-          <a>Your account</a></router-link
-        >
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <router-link to="/" class="navbar-brand">
+      <img
+        src="https://onix.design/img/planet-2.svg"
+        width="30"
+        height="30"
+        alt=""
+    /></router-link>
+    <ul class="nav navbar-nav">
+      <router-link to="/list" activeClass="active" tag="li">
+        <a>BOOK LIST</a></router-link
+      >
+      <router-link to="/account" activeClass="active" tag="li">
+        <a>Your account</a></router-link
+      >
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+        <p style="color:white">
+          <img
+            src="../assets/cart-plus-solid.svg"
+            width="20"
+            height="20"
+            alt=""
+          />Cart: {{ cart }}
+        </p>
+      </li>
+    </ul>
   </nav>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      dropDown: false
+    };
+  },
+  computed: {
+    cart() {
+      return this.$store.getters.cart;
+    }
+  }
+};
+</script>
 <style>
-a {
+a,
+p {
   font-size: 20px;
   line-height: 20px;
   text-align: center;
